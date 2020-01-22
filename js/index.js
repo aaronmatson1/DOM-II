9e2b739804c1719fb7eff8b92eec4848a7b61d63
aaronmatson1 <matsonaaron1@gmail.com>
2020-01-21 20:30:23 -0700

    third event listener. Hover over button text change.

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@
 const logoHeading = document.querySelector(".logo-heading");
 const introImg = document.querySelector("header img");
 const buttons = document.querySelectorAll(".btn");
+const contentPick = document.querySelector(".content-pick");
+
+window.addEventListener("load", function(e) {
+    console.log("All resources finished loading!");
+  });
 
 logoHeading.addEventListener("mouseover", function() {
     introImg.style.display = "initial";
@@ -10,6 +15,7 @@ logoHeading.addEventListener("mouseover", function() {
 buttons.forEach((btn) => {
     btn.addEventListener("mouseover", function(e) {
         e.target.style.transform = "scale(1.2,1.2)";
+        e.target.textContent = "Click for adventure";
     })
     // btn.removeEventListener("mouseover", function(e) {
     //     e.target.style.transform = "scale(1.2,1.2)";
