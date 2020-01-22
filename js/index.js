e1f43f16f722afcf089ddeaf094a988233694da5
aaronmatson1 <matsonaaron1@gmail.com>
2020-01-21 20:25:56 -0700

    first event listener.

--- js/index.js
+++ js/index.js
@@ -1,42 +1,8 @@
 // Your code goes here
 
-const btns = document.querySelectorAll('.destination .btn');
-btns.forEach(function (btn) {
-    btn.addEventListener("mouseover", (e) => {
-        e.target.style.color = "black";
-        e.target.style.background = "white";
-        e.target.style.borderRadius = "0px";
-    });
-});
+const logoHeading = document.querySelector(".logo-heading");
+const introImg = document.querySelector("header img");
 
-const navlink = document.querySelectorAll('a.nav-link');
-navlink.forEach(link => {
-  link.addEventListener('mouseenter', e => {
-    e.target.style.color = 'red';
-  });
-  link.addEventListener('mouseleave', e => {
-    e.target.style.color = 'black';
+logoHeading.addEventListener('mouseover', function(e) {
+    introImg.style.display = "initial";
 })
-
-
-document.addEventListener('keydown', (event) => {
-    const keyName = event.key;
-    const prize1 = (Math.random()) * 100;
-    const prize = Math.round(prize1);
-    alert(`You pressed ${keyName} and your prize is ${prize} % off!`);
-});
-
-document.addEventListener('keyup', (event) => {
-    const keyName = event.key;
-    alert(`You pressed ${keyName} and your prize is ${prize} % off!`);
-});
-
-document.addEventListener('keyleft', (event) => {
-    const keyName = event.key;
-    alert(`You pressed ${keyName} and your prize is ${prize} % off!`);
-});
-
-document.addEventListener('keyright', (event) => {
-    const keyName = event.key;
-    alert(`You pressed ${keyName} and your prize is ${prize} % off!`);
-});
